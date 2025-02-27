566fad98c354100ff4c3594cf6c5e1afdf446057
Benjamin Johnson <benjamin.johnson@Benjamins-MacBook-Air.local>
2025-02-27 09:29:06 -0700

    Finished Step 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// Benjamin Johnson, 2/26, Lab 4 - Mad Libs
 Console.Clear();
+
+Console.WriteLine(@"
+Welcome to 'Mad Libs'
+I will ask for different types of words with which I will build a funny story with.");
+
--- obj/Debug/net9.0/lab-4-madlibs-1-BenjaminJ-146.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-4-madlibs-1-BenjaminJ-146.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-4-madlibs-1-BenjaminJ-146")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e709f413b7dee1f656e480efb86481677e8dc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf77a8df8a33427398e21c4230a110ec3fb87f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-4-madlibs-1-BenjaminJ-146")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-4-madlibs-1-BenjaminJ-146")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
