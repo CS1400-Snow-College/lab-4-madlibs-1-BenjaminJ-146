57329f479205038c936f708e9f2b9c5f6a5dc6cd
Benjamin Johnson <benjamin.johnson@Benjamins-MacBook-Air.local>
2025-03-04 08:37:39 -0700

    Finished Step 3

--- obj/Debug/net9.0/lab-4-madlibs-1-BenjaminJ-146.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-4-madlibs-1-BenjaminJ-146.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-4-madlibs-1-BenjaminJ-146")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf77a8df8a33427398e21c4230a110ec3fb87f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566fad98c354100ff4c3594cf6c5e1afdf446057")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-4-madlibs-1-BenjaminJ-146")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-4-madlibs-1-BenjaminJ-146")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
